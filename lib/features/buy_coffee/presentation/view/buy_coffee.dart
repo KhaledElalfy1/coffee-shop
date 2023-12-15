4dceea9934c8d3e8f38613d08870e2b63fa20df7
KhaledElalfy1 <khaledalfy53@gmail.com>
2023-12-15 14:36:11 +0200

    finsish address part and add custom widgets

--- lib/features/buy_coffee/presentation/view/buy_coffee.dart
+++ lib/features/buy_coffee/presentation/view/buy_coffee.dart
@@ -1,26 +1,17 @@
 import 'package:coffe_shop/core/helpers/extentions.dart';
 import 'package:coffe_shop/core/models/coffee_model.dart';
-import 'package:coffe_shop/core/utils/app_color.dart';
-
 import 'package:coffe_shop/core/utils/fonts.dart';
-import 'package:coffe_shop/features/buy_coffee/presentation/view/widgets/delevry_action.dart';
-import 'package:coffe_shop/features/buy_coffee/presentation/view/widgets/delivery_kind.dart';
+import 'package:coffe_shop/features/buy_coffee/presentation/view/widgets/custom_buy_container.dart';
+import 'package:coffe_shop/features/buy_coffee/presentation/view/widgets/toggle_button.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+
 import 'package:gap/gap.dart';
 
-class BuyCoffee extends StatefulWidget {
+class BuyCoffee extends StatelessWidget {
   const BuyCoffee({super.key, required this.coffeeModel});
   final CoffeeModel coffeeModel;
 
-  @override
-  State<BuyCoffee> createState() => _BuyCoffeeState();
-}
-
-class _BuyCoffeeState extends State<BuyCoffee> {
-  List<bool> isSelected = [true, false];
-  bool vertical = false; // for rotation
-  List<Text> buttons = const [Text('Delivered'), Text('Pick Up')];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -41,41 +32,41 @@ class _BuyCoffeeState extends State<BuyCoffee> {
         child: ListView(
           children: [
             Gap(25.h),
-            Container(
-              child: ToggleButtons(
-                  direction: vertical ? Axis.vertical : Axis.horizontal,
-                  isSelected: isSelected,
-                  onPressed: (index) {
-                    setState(() {
-                      for (var i = 0; i < isSelected.length; i++) {
-                        isSelected[i] = i == index;
-                      }
-                    });
-                  },
-                  color: Colors.black, // unselected item color
-                  borderRadius: BorderRadius.circular(14.r),
-                  fillColor: ColorManger.switchColor,
-                  selectedColor: Colors.white,
-                  constraints: BoxConstraints(
-                    minHeight: 48.0.h,
-                    minWidth: 150.0.w,
-                  ),
-                  children: buttons
-                  //  [
-                  //   ActionContainer(
-                  //     color: ColorManger.switchColor,
-                  //     text: 'Deliver',
-                  //     textStyle: AppFonts.semiBold16LightBlack
-                  //         .copyWith(color: Colors.white),
-                  //   ),
-                  //   ActionContainer(
-                  //     color: ColorManger.gary,
-                  //     text: 'Pick up',
-                  //     textStyle: AppFonts.regular16Black,
-                  //   ),
-                  // ],
-                  ),
-            )
+            const CustomToggleButton(),
+            Gap(24.h),
+            Text(
+              'Delivery Address',
+              style: AppFonts.semiBold18Black,
+            ),
+            Gap(16.h),
+            Text(
+              'Jl. Kpg Sutoyo',
+              style: AppFonts.semiBold14whit.copyWith(
+                color: const Color(0xff303336),
+              ),
+            ),
+            Text(
+              'Kpg. Sutoyo No. 620, Bilzen, Tanjungbalai.',
+              style: AppFonts.regular12LightBlack,
+            ),
+            Gap(16.h),
+            Row(
+              children: [
+                const CustomOrderContainer(
+                  imageSvgPAth: 'assets/images/edit.svg',
+                  text: 'Edit Address',
+                ),
+                Gap(9.w),
+                const CustomOrderContainer(
+                  imageSvgPAth: 'assets/images/note.svg',
+                  text: 'Add Note',
+                ),
+              ],
+            ),
+            const Divider(
+              color: Color(0xffEAEAEA),
+              thickness: 2,
+            ),
           ],
         ),
       ),
