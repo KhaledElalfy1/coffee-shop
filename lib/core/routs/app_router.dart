5ac27fd048eb80d9f318360490b76795e6511dd1
KhaledElalfy1 <khaledalfy53@gmail.com>
2023-12-25 07:44:57 +0200

    add & finish pick coffe size cubit

--- lib/core/routs/app_router.dart
+++ lib/core/routs/app_router.dart
@@ -2,10 +2,12 @@ import 'package:coffe_shop/core/models/coffee_model.dart';
 import 'package:coffe_shop/core/routs/routing.dart';
 import 'package:coffe_shop/features/buy_coffee/presentation/view/buy_coffee.dart';
 import 'package:coffe_shop/features/details/presentation/view/details_view.dart';
+import 'package:coffe_shop/features/details/presentation/view_model/pick_coffee_size_cubit/pick_coffee_size_cubit.dart';
 import 'package:coffe_shop/features/home/presentation/view/home.dart';
 import 'package:coffe_shop/features/no_route/no_route.dart';
 import 'package:coffe_shop/features/onboarding/presentation/view/onboarding.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class AppRouter {
   Route generateRoute(RouteSettings settings) {
@@ -21,14 +23,17 @@ class AppRouter {
         );
       case Routing.detailView:
         return MaterialPageRoute(
-          builder: (_) =>  DetailsView(coffeeModel: arguments as CoffeeModel),
+          builder: (_) => BlocProvider(
+            create: (context) => PickCoffeeSizeCubit(),
+            child: DetailsView(coffeeModel: arguments as CoffeeModel),
+          ),
         );
       case Routing.buyCoffeeView:
         return MaterialPageRoute(
-          builder: (_) =>  BuyCoffee(coffeeModel: arguments as CoffeeModel),
+          builder: (_) => BuyCoffee(coffeeModel: arguments as CoffeeModel),
         );
 
-        default:
+      default:
         return MaterialPageRoute(
           builder: (_) => const NoRouting(),
         );
